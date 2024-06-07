14edf2a765ee86d4f8270eb872f79daa8e39f688
frectonz <fraol0912@gmail.com>
2024-06-07 10:51:55 +0300

    parse processes list

--- bin/main.ml
+++ bin/main.ml
@@ -1,4 +1,38 @@
+module Parser = struct
+  open Angstrom
+
+  let pid =
+    take_while1 (function '0' .. '9' -> true | _ -> false) >>| int_of_string
+
+  let user =
+    take_while1 (function 'a' .. 'z' | 'A' .. 'Z' -> true | _ -> false)
+
+  let command = take_while (function '\n' -> false | _ -> true)
+  let spaces = skip_while (function ' ' -> true | _ -> false)
+
+  let line =
+    let* _ = spaces in
+    let* pid = pid in
+    let* _ = spaces in
+    let* user = user in
+    let* _ = spaces in
+    let* command = command in
+    return (pid, user, command)
+
+  let input = many (line <* end_of_line)
+  let parse s = parse_string ~consume:All input s
+end
+
 let () =
-  Feather.process "ps" [ "-e" ]
-  |> Feather.collect Feather.stdout
-  |> print_endline
+  match
+    Feather.process "ps" [ "-eo"; "pid,user,cmd"; "--no-headers"; "-ww" ]
+    |> Feather.collect Feather.stdout
+    |> (fun s -> s ^ "\n")
+    |> Parser.parse
+  with
+  | Ok procs ->
+      List.iter
+        (fun (pid, user, command) ->
+          Printf.printf "pid=[%d] user=[%s] commad=[%s]\n" pid user command)
+        procs
+  | Error err -> Printf.printf "Error: %s\n" err
